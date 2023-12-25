782ff5185f1b99fbd7651b9ee4026de12834a6e6
darwannneee <darwanchannel1@gmail.com>
2023-12-25 19:18:47 +0700

    changes title and added logo

--- src/assets/Components/Roadmap.js
+++ src/assets/Components/Roadmap.js
@@ -1,7 +1,7 @@
 function Roadmap() {
     return (
         <main className="bg-[url('./assets/img/RoadmapHandphone.png')] md:bg-[url('./assets/img/Roadmap.png')] h-[730px] md:h-screen bg-cover">
-            <div className="pt-20 xl:pt-52">
+            <div className="pt-24 xl:pt-44">
                 <h1 className="text-white fontTitle text-5xl md:text-[85px] xl:text-9xl text-center tracking-wider">ROADMAP</h1>
             </div>  
         </main>
