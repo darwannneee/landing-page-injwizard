fdc707bc88a5e8e8135c49a3c8f32d070cf21268
darwannneee <darwanchannel1@gmail.com>
2023-12-25 21:14:25 +0700

    changes responsive

--- src/assets/Components/Header.js
+++ src/assets/Components/Header.js
@@ -1,6 +1,6 @@
 function Header() {
     return (
-        <main className="bg-[url('./assets/img/HeaderHandphone.gif')] md:bg-[url('./assets/img/Header.gif')] h-[730px] md:h-screen bg-cover">
+        <main className="bg-[url('./assets/img/HeaderHandphone.gif')] md:bg-[url('./assets/img/Header.gif')] h-[730px] xl:h-[1400px] md:h-screen bg-cover">
             <div className="pt-64 md:pt-72 xl:pt-96">
                 <h1 className="text-white fontTitle text-5xl text-center tracking-wider md:pt-7 md:text-[85px] xl:pt-11 xl:text-8xl dekstop:pt-44 dekstop:text-9xl">
                     INJ WIZARD
--- src/assets/Components/MoreSneakPeaks.js
+++ src/assets/Components/MoreSneakPeaks.js
@@ -1,6 +1,6 @@
 function MoreSneakPeaks() {
     return (
-        <main className="bg-[url('./assets/img/MoreSneakPeaksHandphone.gif')] md:bg-[url('./assets/img/MoreSneakPeaks.gif')] h-[730px] md:h-screen bg-cover">
+        <main className="bg-[url('./assets/img/MoreSneakPeaksHandphone.gif')] md:bg-[url('./assets/img/MoreSneakPeaks.gif')] h-[730px] md:h-screen xl:h-[1400px] bg-cover">
         
         </main>
     )
