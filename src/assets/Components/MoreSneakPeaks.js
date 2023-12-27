49e9d7341c28f42c08922d0e88667b3c13cdc98f
darwannneee <darwanchannel1@gmail.com>
2023-12-27 21:58:26 +0700

    latest update

--- src/assets/Components/MoreSneakPeaks.js
+++ src/assets/Components/MoreSneakPeaks.js
@@ -1,6 +1,6 @@
 function MoreSneakPeaks() {
     return (
-        <main className="bg-[url('./assets/img/MoreSneakPeaksHandphone.gif')] md:bg-[url('./assets/img/MoreSneakPeaks.gif')] h-[730px] md:h-screen md2:h-[810px] xl:h-[1400px] bg-cover bg-no-repeat bg-center">
+        <main className="bg-[url('./assets/img/MoreSneakPeaksHandphone.gif')] md:bg-[url('./assets/img/MoreSneakPeaks.gif')] h-[730px] bg-cover bg-no-repeat bg-center">
         
         </main>
     )
--- src/assets/Components/SneakPeaks.js
+++ src/assets/Components/SneakPeaks.js
@@ -1,6 +1,6 @@
 function SneakPeaks() {
     return (
-        <main className="bg-[url('./assets/img/SneakPeaksHandphone.gif')] md:bg-[url('./assets/img/SneakPeaks.gif')] h-[730px] xl:h-[1400px] md2:h-[810px] md:h-screen bg-cover bg-no-repeat bg-center">
+        <main className="bg-[url('./assets/img/SneakPeaksHandphone.gif')] md:bg-[url('./assets/img/SneakPeaks.gif')] h-[730px] bg-cover bg-no-repeat bg-center">
     
         </main>
     )
