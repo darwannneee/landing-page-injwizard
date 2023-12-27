e193c6864f5dea8a6f09f5f66de958eeea0c2896
darwannneee <darwanchannel1@gmail.com>
2023-12-27 21:53:40 +0700

    changes

--- src/assets/Components/Header.js
+++ src/assets/Components/Header.js
@@ -1,6 +1,6 @@
 function Header() {
     return (
-        <main className="bg-[url('./assets/img/HeaderHandphone.gif')] md:bg-[url('./assets/img/Header.gif')] h-[730px] xl:h-[1400px] md:h-56 bg-cover bg-no-repeat bg-center md2:h-[810px]">
+        <main className="bg-[url('./assets/img/HeaderHandphone.gif')] md:bg-[url('./assets/img/Header.gif')] h-[730px] bg-cover bg-no-repeat bg-center">
             
         </main>
     )
--- src/assets/Components/Roadmap.js
+++ src/assets/Components/Roadmap.js
@@ -1,6 +1,6 @@
 function Roadmap() {
     return (
-        <main className="bg-[url('./assets/img/RoadmapHandphone.png')] md:bg-[url('./assets/img/Roadmap.gif')] h-[730px] md2:h-[810px] md:h-screen bg-cover bg-no-repeat bg-center">
+        <main className="bg-[url('./assets/img/RoadmapHandphone.png')] md:bg-[url('./assets/img/Roadmap.gif')] h-[730px] bg-cover bg-no-repeat bg-center">
              
         </main>
     )
