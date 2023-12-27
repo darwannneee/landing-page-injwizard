0d328ab3a2c7c88a2debaab55dc794f6c7decc49
darwannneee <darwanchannel1@gmail.com>
2023-12-27 21:47:00 +0700

    changes image

--- src/assets/Components/Header.js
+++ src/assets/Components/Header.js
@@ -1,11 +1,7 @@
 function Header() {
     return (
-        <main className="bg-[url('./assets/img/HeaderHandphone.gif')] md:bg-[url('./assets/img/Header.gif')] h-[730px] xl:h-[1400px] md:h-screen md2:h-[810px] bg-cover">
-            <div className="pt-64 md:pt-72 xl:pt-96">
-                <h1 className="text-white fontTitle text-5xl text-center tracking-wider md:pt-7 md2:pt-6 md:text-[85px] xl:pt-11 xl:text-8xl dekstop:pt-44 dekstop:text-9xl">
-                    INJ WIZARD
-                </h1>
-            </div>
+        <main className="bg-[url('./assets/img/HeaderHandphone.gif')] md:bg-[url('./assets/img/Header.gif')] h-[730px] xl:h-[1400px] md:h-56 bg-cover bg-no-repeat bg-center md2:h-[810px]">
+            
         </main>
     )
 }
--- src/assets/Components/Roadmap.js
+++ src/assets/Components/Roadmap.js
@@ -1,9 +1,7 @@
 function Roadmap() {
     return (
-        <main className="bg-[url('./assets/img/RoadmapHandphone.png')] md:bg-[url('./assets/img/Roadmap.png')] h-[730px] md2:h-[810px] md:h-screen bg-cover">
-            <div className="pt-24 xl:pt-32 dekstop:pt-40 ">
-                <h1 className="text-white fontTitle text-5xl md:text-[85px] xl:text-9xl text-center tracking-wider">ROADMAP</h1>
-            </div>  
+        <main className="bg-[url('./assets/img/RoadmapHandphone.png')] md:bg-[url('./assets/img/Roadmap.gif')] h-[730px] md2:h-[810px] md:h-screen bg-cover bg-no-repeat bg-center">
+             
         </main>
     )
 }
