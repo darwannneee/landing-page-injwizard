70ef2935ce08db22fb6f0e4d413d3f3346b6f65d
darwannneee <darwanchannel1@gmail.com>
2024-01-09 23:51:18 +0700

    edited roadmap

--- src/assets/Components/Header.js
+++ src/assets/Components/Header.js
@@ -28,37 +28,37 @@ function Header() {
                 alt="Header Image"
             />
 
-<div className="absolute inset-0 flex items-center justify-center pt-32 dekstop:pt-40">
-      <button
-        className="w-44 h-16 dekstop:w-56 dekstop:h-20 mx-2 bg-cover bg-center relative hover:w-48 hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
-        style={{
-            backgroundImage: `url(${Button})`,
-            filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.6))', // Sesuaikan bayangan sesuai kebutuhan
-        }}
-      >
-        <span className="fontTitle text-white text-2xl dekstop:text-3xl">DISCORD</span>
-      </button>
+            <div className="absolute inset-0 flex items-center justify-center pt-32 dekstop:pt-40">
+                <button
+                    className="w-44 h-16 dekstop:w-56 dekstop:h-20 mx-2 bg-cover bg-center relative hover:w-48 hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
+                    style={{
+                        backgroundImage: `url(${Button})`,
+                        filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.6))', // Sesuaikan bayangan sesuai kebutuhan
+                    }}
+                >
+                    <span className="fontTitle text-white text-2xl dekstop:text-3xl">DISCORD</span>
+                </button>
 
-      <button
-        className="w-44 h-16 dekstop:w-56 dekstop:h-20 bg-cover bg-center relative mx-28 dekstop:mx-48 hover:w-48 hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
-        style={{
-            backgroundImage: `url(${Button})`,
-            filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.6))', // Sesuaikan bayangan sesuai kebutuhan
-        }}
-      >
-        <span className="fontTitle text-white text-2xl dekstop:text-3xl">CHECKER</span>
-      </button>
+                <button
+                    className="w-44 h-16 dekstop:w-56 dekstop:h-20 bg-cover bg-center relative mx-28 dekstop:mx-48 hover:w-48 hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
+                    style={{
+                        backgroundImage: `url(${Button})`,
+                        filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.6))', // Sesuaikan bayangan sesuai kebutuhan
+                    }}
+                >
+                    <span className="fontTitle text-white text-2xl dekstop:text-3xl">CHECKER</span>
+                </button>
 
-      <button
-        className="w-44 h-16 dekstop:w-56 dekstop:h-20 mx-2 bg-cover bg-center relative hover:w-48 hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
-        style={{
-            backgroundImage: `url(${Button})`,
-            filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.6))', // Sesuaikan bayangan sesuai kebutuhan
-        }}
-      >
-        <span className="fontTitle text-white text-2xl dekstop:text-3xl">TWITTER</span>
-      </button>
-    </div>
+                <button
+                    className="w-44 h-16 dekstop:w-56 dekstop:h-20 mx-2 bg-cover bg-center relative hover:w-48 hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
+                    style={{
+                        backgroundImage: `url(${Button})`,
+                        filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.6))', // Sesuaikan bayangan sesuai kebutuhan
+                    }}
+                >
+                    <span className="fontTitle text-white text-2xl dekstop:text-3xl">TWITTER</span>
+                </button>
+            </div>
         </div>
     
     )
--- src/assets/Components/Roadmap.js
+++ src/assets/Components/Roadmap.js
@@ -2,7 +2,7 @@ import Footer from "../img/Footer.gif"
 function Roadmap() {
     return (
         <main className="min-h-screen flex flex-col items-center relative">
-            <img src={Footer} className="lg:block bottom-0 transform object-cover -z-10 prevent-drag h-screen" alt="Footer Image" />
+            <img src={Footer} className="lg:block bottom-0 transform object-cover -z-10 prevent-drag h-screen w-full" alt="Footer Image" />
             <div className="absolute inset-0 flex justify-center pt-16">
                 <h1 className="text-white text-6xl font-bold fontTitle">Roadmap ComingSoon</h1>
             </div>
