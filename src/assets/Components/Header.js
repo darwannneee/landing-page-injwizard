243e4dd8b75b386ce3ed1a9e61dc6fdd532888a6
darwannneee <darwanchannel1@gmail.com>
2024-01-09 23:20:30 +0700

    added navbar

--- src/assets/Components/Header.js
+++ src/assets/Components/Header.js
@@ -1,5 +1,6 @@
 import HeaderImage from "../img/Header.gif"
 import HeaderMobile from "../img/HeaderHandphone.gif"
+import Button from "../img/Button.png"
 
 import { useEffect, useState } from "react";
 function Header() {
@@ -20,9 +21,36 @@ function Header() {
         };
     }, []);
     return (
-        <main class="min-h-screen flex">
-            <img src={isMobile ? HeaderMobile : HeaderImage} class="lg:block bottom-0 transform object-cover -z-10 prevent-drag h-screen w-full" alt="Header Image" />
-        </main>
+        <div className="relative">
+            <img
+                src={isMobile ? HeaderMobile : HeaderImage}
+                className="lg:block bottom-0 transform object-cover -z-10 prevent-drag h-screen w-full"
+                alt="Header Image"
+            />
+
+<div className="absolute inset-0 flex items-center justify-center pt-32">
+      <button
+        className="w-44 h-16 mx-2 bg-cover bg-center relative hover:w-48 hover:h-20 hover:shadow-md"
+        style={{ backgroundImage: `url(${Button})` }}
+      >
+        <span className="fontTitle text-white text-2xl">DISCORD</span>
+      </button>
+
+      <button
+        className="w-44 h-16 bg-cover bg-center relative mx-28 hover:w-48 hover:h-20 hover:shadow-md"
+        style={{ backgroundImage: `url(${Button})` }}
+      >
+        <span className="fontTitle text-white text-2xl">CHECKER</span>
+      </button>
+
+      <button
+        className="w-44 h-16 mx-2 bg-cover bg-center relative hover:w-48 hover:h-20 hover:shadow-md"
+        style={{ backgroundImage: `url(${Button})` }}
+      >
+        <span className="fontTitle text-white text-2xl">TWITTER</span>
+      </button>
+    </div>
+        </div>
     
     )
 }
