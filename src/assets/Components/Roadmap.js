ff0a448b611928c1c1bf78cd6536e5dd7f93b9c5
darwannneee <darwanchannel1@gmail.com>
2024-01-10 12:19:14 +0700

    fixed footer

--- src/assets/Components/Roadmap.js
+++ src/assets/Components/Roadmap.js
@@ -21,7 +21,7 @@ function SneakPeaks() {
     }, []);
 
     return (
-        <main class="absolute h-screen flex">
+        <main class="relative">
             <img src={isMobile ? FooterMobile : Footer} class="lg:block bottom-0 transform bg-cover -z-10 prevent-drag h-screen w-full object-cover" alt="Header Image" />
             <div className="absolute inset-0 flex justify-center pt-20 md:pt-16">
                 <h1 className="text-white mx-11 text-center text-5xl md:text-6xl font-bold fontTitle">Roadmap ComingSoon</h1>
