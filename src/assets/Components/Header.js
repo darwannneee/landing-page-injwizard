6e9545f219ccf310eba3d6f42798d1ddc154a506
darwannneee <darwanchannel1@gmail.com>
2024-01-14 02:56:34 +0700

    fixed button hover

--- src/assets/Components/Header.js
+++ src/assets/Components/Header.js
@@ -31,7 +31,7 @@ function Header() {
             <div className="absolute inset-0 md:flex items-center justify-center pt-96 mx-10 md:mx-0 text-center md:pt-32 dekstop:pt-40">
                 <a href="https://discord.gg/CMpDT4WZHt">
                     <button
-                        className="w-40 h-16 md:w-44 md:h-16 dekstop:w-56 dekstop:h-20 mx-2 bg-cover bg-center relative hover:w-48 hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
+                        className="w-40 h-16 md:w-44 md:h-16 dekstop:w-56 dekstop:h-20 mx-2 bg-cover bg-center relative md:hover:w-48 md:hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
                         style={{
                             backgroundImage: `url(${Button})`,
                             filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.6))', // Sesuaikan bayangan sesuai kebutuhan
