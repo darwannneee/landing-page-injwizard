57eb7d004c00120dabf277599b88ab72f4125326
darwannneee <darwanchannel1@gmail.com>
2024-01-13 18:07:37 +0700

    added link

--- src/assets/Components/Header.js
+++ src/assets/Components/Header.js
@@ -29,16 +29,19 @@ function Header() {
             />
 
             <div className="absolute inset-0 md:flex items-center justify-center pt-96 mx-10 md:mx-0 text-center md:pt-32 dekstop:pt-40">
-                <button
-                    className="w-40 h-16 md:w-44 md:h-16 dekstop:w-56 dekstop:h-20 mx-2 bg-cover bg-center relative hover:w-48 hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
-                    style={{
-                        backgroundImage: `url(${Button})`,
-                        filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.6))', // Sesuaikan bayangan sesuai kebutuhan
-                    }}
-                >
-                    <span className="fontTitle text-white md:text-2xl dekstop:text-3xl">DISCORD</span>
-                </button>
-
+                <a href="https://discord.gg/CMpDT4WZHt">
+                    <button
+                        className="w-40 h-16 md:w-44 md:h-16 dekstop:w-56 dekstop:h-20 mx-2 bg-cover bg-center relative hover:w-48 hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
+                        style={{
+                            backgroundImage: `url(${Button})`,
+                            filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.6))', // Sesuaikan bayangan sesuai kebutuhan
+                        }}
+                    >
+                        <span className="fontTitle text-white md:text-2xl dekstop:text-3xl">DISCORD</span>
+                    </button>
+                </a>
+                
+                        
                 <button
                     className="w-40 h-16 md:w-44 mt-5 md:mt-0 md:h-16 dekstop:w-56 dekstop:h-20 bg-cover bg-center relative md:mx-28 dekstop:mx-48 hover:w-48 hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
                     style={{
@@ -49,15 +52,18 @@ function Header() {
                     <span className="fontTitle text-white md:text-2xl dekstop:text-3xl">CHECKER</span>
                 </button>
 
-                <button
-                    className="w-40 h-16 md:w-44 mt-5 md:mt-0 md:h-16 dekstop:w-56 dekstop:h-20 mx-2 bg-cover bg-center relative hover:w-48 hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
+                <a href="https://twitter.com/Inject_Wizard">
+                    <button
+                    className="w-40 h-16 md:w-44 mt-5 md:mt-0 md:h-16 dekstop:w-56 dekstop:h-20 mx-2 bg-cover bg-center relative md:hover:w-48 md:hover:h-20 dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md"
                     style={{
                         backgroundImage: `url(${Button})`,
                         filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.6))', // Sesuaikan bayangan sesuai kebutuhan
                     }}
-                >
-                    <span className="fontTitle text-white md:text-2xl dekstop:text-3xl">TWITTER</span>
-                </button>
+                    >
+                        <span className="fontTitle text-white md:text-2xl dekstop:text-3xl">TWITTER</span>
+                    </button>
+                </a>
+                
             </div>
 
         </div>
