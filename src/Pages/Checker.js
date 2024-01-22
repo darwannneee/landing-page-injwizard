4ada4d1016aa8e362006bc3abfe006bd272525ec
darwannneee <darwanchannel1@gmail.com>
2024-01-22 20:11:14 +0700

    updated checker

--- src/Pages/Checker.js
+++ src/Pages/Checker.js
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import WalletChecker from "../assets/img/Wallet_Checker.gif"
 import CheckerMobile from "../assets/img/Checker_mobile.gif"
 import Button from "../assets/img/Button.png"
+import WhitelistData from "../assets/data/wl.json"
+import OgData from "../assets/data/og.json"
 
 function Checker() {
     const [isMobile, setIsMobile] = useState(false);
+    const [status, setStatus] = useState("none")
 
     useEffect(() => {
         const handleResize = () => {
@@ -20,22 +23,72 @@ function Checker() {
         window.removeEventListener('resize', handleResize);
         };
     }, []);
+
+    const handleOnchange = (event) => {
+        const address = event.target.value;
+        
+        if (address.trim() !== "") {
+            if(WhitelistData.find(data => data === address)) {
+                setStatus('wl')
+            } 
+            else if(OgData.find(data => data === address)) {
+                setStatus('og')
+            } 
+            else {
+                setStatus("no");
+            }
+        } else {
+            setStatus("none");
+        }
+    }    
+
+    console.log(status)
+
+
+
     return (
         <div>
             <main class="h-screen flex">
                 <img src={isMobile ? CheckerMobile : WalletChecker} class="lg:block bottom-0 transform bg-cover -z-10 prevent-drag h-screen w-full object-cover" alt="Header Image" />
                 <div className="absolute inset-0 flex flex-col items-center pt-[245px] md:pt-56">
                     <h1 className="text-white text-center text-3xl md:text-4xl font-bold fontTitle">Wallet Checker</h1>
-                    <input className="w-48 h-7 md:w-80 md:h-11 mt-4 md:mt-16 rounded-2xl text-center"></input>
-                    <button
+                    <input className="w-48 h-7 md:w-80 md:h-11 mt-4 md:mt-16 rounded-2xl text-center" name="inputAddress" onInput={handleOnchange}></input>
+                    {/* <button
                     className="w-16 h-7 md:w-28 md:mt-3 md:h-10 dekstop:w-56 dekstop:h-20 mx-2 bg-cover bg-center relative  dekstop:hover:w-60 dekstop:hover:h-24 hover:shadow-md mt-1"
                     style={{
                         backgroundImage: `url(${Button})`,
                         filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.6))', // Sesuaikan bayangan sesuai kebutuhan
                     }}
+                    onClick={() => setAddress()}
                     >
                         <span className="fontTitle text-white text-xs md:text-xl dekstop:text-3xl">check</span>
-                    </button>
+                    </button> */}
+
+                {status === 'og' && (
+                    <div>
+                        <h1 className="text-white text-8xl">you're OG</h1>
+                    </div>
+                )}
+
+                {status === 'wl' && (
+                    <div>
+                        <h1 className="text-white text-8xl">you're WL</h1>
+                    </div>
+                )}
+
+                {status === 'no' && (
+                    <div>
+                       <h1 className="text-white text-8xl">sorry, you can mint at public phase</h1>
+                    </div>
+                )}
+
+                {status === 'none' && (
+                    <div>
+                        <h1 className="text-white text-8xl"></h1>
+                    </div>
+                )}
+                
+
                 </div>
             </main>
         </div>
